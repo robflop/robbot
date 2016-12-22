007c6462893402b6fdf9bc21935a92a86564578a
robflop <robflop98@gmail.com>
2016-12-22 23:14:43 +0100

    Various adjustments

--- app.js
+++ app.js
@@ -20,88 +20,62 @@ bot.on('message', msg => {
         });
     };
 	if(msg.content == "!submit") {
-		msg.channel.sendMessage("Want to submit a sound for the website? No problem, send me an email at `me@robflop.pw` including your cropped mp3/aac/ogg sound file! All sounds need to fit the website's theme.");	
+		msg.channel.sendMessage("Want to submit a sound for the website/bot? No problem, send me an email at `me@robflop.pw` including your cropped mp3/aac/ogg sound file! All sounds need to fit the website's theme.");	
 	}
 	if(msg.content == "!randomsound") {
 		if(msg.member.voiceChannel){
-			request('https://megumin.love/includes/cache_counter.php?count=1', function (error, response, body){
-			}); 
+			request('https://megumin.love/includes/cache_counter.php?count=1', function (error, response, body){}); 
 			msg.member.voiceChannel.join().then(connection => {
-				console.log("Connected to " + msg.member.voiceChannel.name + "!");
-				const random = Math.floor(Math.random() * 12) + 1;
-				var sound;
-				switch(random) {
-            		case 1:
-                		var sound = "yamero.mp3";
-                		break;
-            		case 2:
-                		var sound = "pull.mp3";
-						break;
-            		case 3:
-                		var sound = "explosion.mp3";
-                		break;
-					case 4:
-                		var sound = "itai.mp3";
-                		break;
-            		case 5:
-                		var sound = "name.mp3";
-                		break;
-            		case 6:
-                		var sound = "eugh1.mp3";
-                		break;
-            		case 7:
-                		var sound = "eugh2.mp3";
-                		break;
-            		case 8: 
-						var sound = "eugh3.mp3";
-						break;
-            		case 9:
-						var sound = "eugh4.mp3";
-						break;
-            		case 10:
-						var sound = "n.mp3";
-						break;
-            		case 11:
-						var sound = "plosion.mp3";
-						break;
-            		case 12:
-						var sound = "sion.mp3";
-						break;
-        		};
+				console.log(`Connected to ${msg.member.voiceChannel.name}!`);
+				var sounds = ["eugh1.mp3", "eugh2.mp3", "eugh3.mp3", "eugh4.mp3", "explosion.mp3", "itai.mp3", "n.mp3", "name.mp3", "plosion.mp3", "pull.mp3", "sion.mp3", "yamero.mp3"]; 
+				var sound = sounds[Math.floor(Math.random()*sounds.length)];
 				const player = connection.playFile(config.soundPath + sound);
-				console.log("'" + config.soundPath + sound + "' played in " + msg.member.voiceChannel.name + "! (" + msg.author.username + "#" + msg.author.discriminator + ")");
+				console.log(`"${config.soundPath + sound}" played in ${msg.member.voiceChannel.name}! (${msg.author.username}#${msg.author.discriminator})`);
 				player.on('end', () => {
 					msg.member.voiceChannel.leave();
-					console.log("Disconnected from " + msg.member.voiceChannel.name + "!");
+					console.log(`Disconnected from ${msg.member.voiceChannel.name}!`);
 				});
+				player.on('debug', console.log);
 			});
 		}
-		else(msg.reply("join a voice channel first!"));
+		else{
+			msg.reply("join a voice channel first!");
+			return;
+		};
 	};
 	if(msg.content.startsWith("!setGame")) {
-		if(msg.author.id !== "108988529383616512"){
+		if(msg.author.id !== config.ownerID){
 			msg.reply("you are not authorized to use this command!");
-			console.log(msg.author.username + "#" + msg.author.discriminator + " tried to change the bot's game, but failed!");
+			console.log(`${msg.author.username}#${msg.author.discriminator} tried to change the bot's game, but failed!`);
 			return;
 		}
 		else {
 			bot.user.setGame(msg.content.substr(8));
-			msg.reply("successfully set my game to '" + msg.content.substr(8) + " ' !");
-			console.log(bot.user.username + "'s game set to \'" + msg.content.substr(8) + " \' !" + " (" + msg.author.username + "#" + msg.author.discriminator + ")")
+			msg.reply(`successfully set my game to '${msg.content.substr(8)}' !`);
+			console.log(`${bot.user.username}'s game set to '${msg.content.substr(8)} ' ! (${msg.author.username}#${msg.author.discriminator})`)
 		};
 	};
 	if(msg.content == "!clearGame") {
-		bot.user.setGame();	
-	};
-	if(msg.content == "!shutdown") {
-		if(msg.author.id !== "108988529383616512"){
+		if(msg.author.id !== config.ownerID){
 			msg.reply("you are not authorized to use this command!");
-			console.log(msg.author.username + "#" + msg.author.discriminator + " tried to shutdown the bot, but failed!");
+			console.log(`${msg.author.username}#${msg.author.discriminator} tried to clear the bot's game, but failed!`);
 			return;
 		}
 		else {
-			msg.reply(bot.user.username + " shutting down! Bye!");
-			console.log(bot.user.username + " shutting down!" + " (" + msg.author.username + "#" + msg.author.discriminator + ")");
+			bot.user.setGame();	
+			console.log(`${bot.user.username}'s game status reset! (${msg.author.username}#${msg.author.discriminator})`);
+			msg.reply("game status cleared!");
+		};
+	};
+	if(msg.content == "!shutdown") {
+		if(msg.author.id !== config.ownerID){
+			msg.reply("you are not authorized to use this command!");
+			console.log(`${msg.author.username}#${msg.author.discriminator} tried to shutdown the bot, but failed!`);
+			return;
+		}
+		else {
+			msg.reply(`${bot.user.username} shutting down! Bye!`);
+			console.log(`${bot.user.username} shutting down! (${msg.author.username}#${msg.author.discriminator})`);
 			setTimeout(function(){process.exit(0)}, 1500);
 		};
 	};
--- readme.md
+++ readme.md
@@ -4,10 +4,12 @@ A discord bot to go along with my website, [megumin.love](https://megumin.love)!
 Written using discord.js, using opusscript and request packages.
 
 ## Usage
-- Adjust config.json to your liking (fill out your bot token!)
+- Fill out your bot token and change the ownerID in config.js to your devmode discord ID.
+- Adjust config.json to your liking
 - ``npm install`` to install all dependencies
 - Run it!
 
 ###### Information
-The default version of the bot uses the ``sounds/`` directory to look for sounds. Change this to your liking.
-Shutdown and gamechange logs are saved in the same directory the base file, app.js, is saved in by default.
+The default version of the bot uses the ``sounds/`` directory to look for sounds.
+Change this to your liking.
+Shutdown, gamechange and sound logs are saved in the same directory the base file, app.js, is saved in by default.
