2fed2e86a7e5822907948440b1fc7f2acb201ad3
robflop <robflop98@gmail.com>
2017-01-07 21:55:30 +0100

    Add invite link to about command

--- commands/about.js
+++ commands/about.js
@@ -8,6 +8,6 @@ exports.main = function(bot, msg, timeout, botPerm, userPerm) { // Export comman
 	var command = "about"; // For logging purposes
 	if(timeout.check(msg.author.id, msg)) { return; }; 
 	// Check for cooldown, if on cooldown notify user of it and abort command execution
-	msg.channel.sendMessage(`robbot made by robflop#2174. Made to complement the website <https://megumin.love> also by robflop#2174.\nCheck out the Github repo at <https://github.com/robflop/megumin.love-discordbot>.`);
+	msg.channel.sendMessage(`robbot made by robflop#2174. Made to complement the website <https://megumin.love> also by robflop#2174.\nCheck out the Github repo at <https://github.com/robflop/megumin.love-discordbot>.\nInvite link for the bot: <https://robflop.pw/robbot>.`);
 };
 exports.desc = "get general info about the bot"; // Export command's description
