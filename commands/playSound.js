ceb8cbbc1abf3740b708dea0c35be603d174057f
robflop <robflop98@gmail.com>
2017-01-12 20:28:34 +0100

    Add two more sounds
    Megumin's "igiari!" (too good not to use) and a "hmph", both from Season
    2 Episode 1.

--- commands/playSound.js
+++ commands/playSound.js
@@ -77,7 +77,7 @@ exports.main = function(bot, msg, timeout, botPerm, userPerm) { // Export comman
 		};
 		// If there is no error, proceed with the command.
 	});
-	var sounds = ["eugh1", "eugh2", "eugh3", "eugh4", "explosion", "itai", "n", "name", "plosion", "pull", "sion", "yamero", "magic-item", "parents", "hyoizaburo", "oi"]; 
+	var sounds = ["eugh1", "eugh2", "eugh3", "eugh4", "explosion", "itai", "n", "name", "plosion", "pull", "sion", "yamero", "magic-item", "parents", "hyoizaburo", "oi", "igiari", "hmph"]; 
 	// Set available files,...
 	var sound = msg.content.substr(config.commandPrefix.length + command.length + 2);
 	// ...then set sound var to chosen sound file.
