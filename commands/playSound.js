e795944de775f266ca8bb7a99316ed7995ab2547
robflop <robflop98@gmail.com>
2017-02-24 21:41:01 +0100

    Switch to discord.js master branch

--- commands/playSound.js
+++ commands/playSound.js
@@ -2,7 +2,6 @@ const config = require('../config.json'); // Import configuration
 const request = require('request'); // For website interaction
 const fs = require('fs'); // For log writing
 const moment = require('moment'); // Part of log writing
-const prism = require('prism-media'); // Prism for smoother file playing of very short files
 
 exports.main = function(bot, msg, cooldown, botPerm, userPerm) { // Export command function
 	var command = "playSound"; // For logging purposes
