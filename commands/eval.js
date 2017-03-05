3ec94103bb6b3ff02526ea284e2559367793773f
robflop <robflop98@gmail.com>
2017-03-05 03:31:27 +0100

    Complete eval rework (by 1Computer1)

--- commands/eval.js
+++ commands/eval.js
@@ -1,4 +1,5 @@
 const config = require('../config.json'); // Import configuration
+const util = require('util'); // For turning objects etc to strings
 
 exports.main = function(bot, msg, cooldown, botPerm, userPerm, chalk) { // Export command's function
     var command = "eval";
@@ -15,35 +16,76 @@ exports.main = function(bot, msg, cooldown, botPerm, userPerm, chalk) { // Expor
     // Define eval input out of the message content
     if(input == '' || input == '"' || input == '""') {msg.channel.sendMessage("No input given.").then(msg => msg.delete(2000)); return;};
     // If input is empty or none was given, notify user and set auto-delete to 2s.
-    try {
-    // Try evaluating the input
-        var output = eval(input);
-        // Define output as the evaluated input
-        if(typeof output !== "string") {
-        // If the output isn't a string (object, array, etc)...
-            output = require('util').inspect(output);
-            // ...inspect it and turn it into a string.
-        };
-        if(typeof output == "string" && output.indexOf(config.token) > -1) {
-        // If the output is a string and contains the bot token...
-            function escapeRegExp(str) {return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");};
-            // ...define function to escape the token's special characters...
-            var escapedToken = escapeRegExp(config.token);
-            // ...define the escaped token under usage of the escape regex function...
-            var regex = new RegExp(escapedToken, "g");
-            // ...define the regex object to check for the bot token from the escaped token...
-            output = output.replace(regex, "<token>");
-            // ...and finally replace all occurences of the user token with "<token>".
-        };
-        msg.channel.sendMessage(`INPUT:\`\`\`js\n${input}\n\`\`\`\n\nOUTPUT: \`\`\`js\n${output}\n\`\`\``, {split: {prepend: "\`\`\`js\n", append: "\`\`\`\n"}});
-        // Send the message with the eval output
-    }
-    catch(error) {
-    // If there is an error evaluating the input...
-        msg.channel.sendMessage(`INPUT:\`\`\`\n${input}\n\`\`\`\n\nERROR: \`\`\`\n${error}\n\`\`\``, {split: {prepend: "\`\`\`\n", append: "\`\`\`\n"}});
-        // ...notify the user.
+    
+    /*
+    Credit for all of the below goes to 1Computer1 on GitHub
+    ily ❤ - from comp
+    */
+  
+	const evaled = {}; // Stores outputs
+    const logs = []; // Stores logs
+
+    const tokenRegex = new RegExp(bot.token.replace(/\./g, '\\.').split('').join('.?'), 'g'); // Regex for tokens
+  
+  	// This is put here instead of outside the command execution because we need a reference to the message and other things
+    const print = (...a) => { // ...a means all arguments
+        const cleaned = a.map(o => {
+            if (typeof o !== 'string') o = util.inspect(o); // Turn to string
+            return o.replace(tokenRegex, '[TOKEN]'); // Replace tokens
+        });
+
+      	// If the evaled object does not have an output, that means the message has not been edited yet
+    	// So we push to the logs array which gets prepended in the then() or catch()
+        if (!evaled.output) return logs.push(...cleaned);
+
+      	// If it is after we evaled, i.e. setTimeout(), then we do this
+      	// The evaled.output has the thing that will be printed to it appended to it, so it persists throughout prints
+        evaled.output += evaled.output.endsWith('\n') ? cleaned.join(' ') : `\n${cleaned.join(' ')}`; // Check for newline, because I'm OCD
+        const title = evaled.errored ? '☠\u2000**Error**' : '📤\u2000**Output**'; // Change title depending on whether it errored or not
+
+        if (evaled.output.length + input.length > 1900) evaled.output = 'Output too long.'; // Make sure it fits
+        return evaled.message.edit(`📥\u2000**Input**${cb}js\n${input}\n${cb}\n${title}${cb}js\n${evaled.output}\n${cb}`); // Re-edit
     };
+
+    var result; // Define result placeholder
+    console.log(msg.content.substring(config.commandPrefix.length + command.length + 2, msg.content.indexOf('"')).trim())
+    if(msg.content.substring(config.commandPrefix.length + command.length + 2, msg.content.indexOf('"')).trim() == "async") {
+        result = new Promise(resolve => resolve(eval(`(async () => { ${input} })()`)));
+        // This version is wrapped in an async function
+  	    // So in order for something to show up in the output, you have to return it
+        // Unlike the other one, the last statement does not show up automatically
+  	    // But it lets you use await!
+    }
+    else {
+        result = new Promise(resolve => resolve(eval(input)));
+        // Non-async promise, no need to return everything manually
+    }
+    const cb = '```'; // Shortcut for codeblock syntax
+  
+  	return result.then(output => {
+        if (typeof output !== 'string') output = util.inspect(output); // Inspect to turn to string if not one
+        output = `${logs.join('\n')}\n${logs.length && output === 'undefined' ? '' : output}`; // Prepend the logs to the output with a check for undefined to make things prettier
+        output = output.replace(tokenRegex, '[TOKEN]'); // Replace tokens
+
+        if (output.length + input.length > 1900) output = 'Output too long.'; // Make sure output fits
+
+        return msg.channel.sendMessage(`📥\u2000**Input**${cb}js\n${input}\n${cb}\n📤\u2000**Output**${cb}js\n${output}\n${cb}`).then(message => { // Edit message
+            evaled.errored = false; // Did not error
+            evaled.output = output; // Save output for later
+            evaled.message = message; // Save msg to edit later
+        });
+    }).catch(err => {
+        console.error(err); // Log error
+        err = err.toString(); // Get error message
+        err = `${logs.join('\n')}\n${logs.length && err === 'undefined' ? '' : err}`; // Prepend the logs
+        err = err.replace(tokenRegex, '[TOKEN]'); // Replace tokens
+
+        return msg.channel.sendMessage(`📥\u2000**Input**${cb}js\n${input}\n${cb}\n☠\u2000**Error**${cb}js\n${err}\n${cb}`).then(() => { // Edit message
+            evaled.errored = true; // Did error
+            evaled.output = err; // Save for later
+        });
+    });
 };
 
-exports.desc = "Evaluate user javascript input -- USE WITH CAUTION. [Bot owner only]"; // Export command description
-exports.syntax = "<input to evaluate enclosed by \"quotes\">"; // Export command syntax 
+exports.desc = "Evaluate javascript input, provide async argument to use async function -- USE COMMAND WITH CAUTION."; // Export command description
+exports.syntax = "<\"async\", optional> <input to evaluate enclosed by \"quotes\">"; // Export command syntax
