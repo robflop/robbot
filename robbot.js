868ce2585b971441250aa86cd9348d700c5336b6
robflop <robflop98@gmail.com>
2017-03-11 12:27:46 +0100

    Fix "more than 11 listeners added" thingy

--- robbot.js
+++ robbot.js
@@ -47,7 +47,7 @@ let cooldown = {
 			cooldown.users.add(userID);
 			// Add userID into the set
             setTimeout(function() {
-			// Set timeout to delete them from the list 
+			// Set timeout to delete them from the list
                 cooldown.users.delete(userID);
 			}, (config.commandCooldown * 1000));
 			// Set to configured amount
@@ -63,7 +63,7 @@ setInterval(function () {
 	else if(bot.user.presence.game.name == "on megumin.love") { // ...but if the current status is...
 		bot.user.setGame(`try '${config.commandPrefix} help' !`); // ...then set it to [...].
 	}
-	else { 
+	else {
 		// however, if the bot's playing status was changed (is neither of two default) or cleared...
 		return;	// ...leave it unchanged.
 	}
@@ -78,7 +78,7 @@ bot.on('message', msg => { // Listen to all messages sent
 		// ...notify the user and abort command execution.
 	};
 	if(msg.content == config.commandPrefix) { return; }; // Ignore empty commands (messages containing just the prefix)
-	if(fs.existsSync(`${config.ignorePath}ignore_${msg.guild.id}.json`)) { 
+	if(fs.existsSync(`${config.ignorePath}ignore_${msg.guild.id}.json`)) {
 	/*
 	Check if an ignore file for the server the command is used on exists
 	(no ignore file exists if the ignore command has not been used yet)
@@ -92,21 +92,21 @@ bot.on('message', msg => { // Listen to all messages sent
 	const botPerm = msg.channel.permissionsFor(bot.user); // For permission checking on the bot's side later on in the commands
 	const userPerm = msg.channel.permissionsFor(msg.member); // For permission checking on the user's side later on in the commands
 	/*
-	INFO: 
+	INFO:
 	Because the commands are all loaded from external files, "bot", "msg", "cooldown", "botPerm", "userPerm", and "chalk" are passed...
 	...to every command by default, whether used by the command or not. Other necessary packages are defined in the command files.
 	Packages not needed for the base file (this one) are only defined in the commands that need them.
-	*/ 
+	*/
 	var actualCmd = msg.content.replace(config.commandPrefix, '').trim().split(' ')[0].toLowerCase();
-	/*	
+	/*
 	Replace (cut out) bot prefix, cut out whitespaces at start and end, split prefix, command
 	and arg into array, convert to lowercase and select the command part ([0] of the array)
 	*/
-	if(fs.existsSync(`${config.serverConfPath}serverconf_${msg.guild.id}.json`)) { 
-	/* 
+	if(fs.existsSync(`${config.serverConfPath}serverconf_${msg.guild.id}.json`)) {
+	/*
 	Check if a disable list for commands for the server the command is used on exists
 	(no disable list exists if the toggle command has not been used yet)
-	*/ 
+	*/
 		if(serverConfig.serverConfig[`serverconf_${msg.guild.id}`].indexOf(actualCmd) > -1) {
 		// Search the disabled commands list of the server the message came from for the command called...
 			return; // ... if it is found, don't execute the command (duh). (Else proceed as usual.)
@@ -119,18 +119,18 @@ bot.on('message', msg => { // Listen to all messages sent
 	};
 	if(actualCmd == "reload") {
 	// Reload command
-		if (cooldown.onCooldown(msg.author.id, msg)) return; 
+		if (cooldown.onCooldown(msg.author.id, msg)) return;
 		// Check for cooldown, if on cooldown notify user of it and abort command execution.
-		if(msg.author.id !== config.ownerID) { 
+		if(msg.author.id !== config.ownerID) {
 			// If the user is not authorized...
 			return msg.reply("you are not authorized to use this command!").then(msg => msg.delete(2000));
 			// ...notify the user and abort command execution.
 		};
 		var arg = msg.content.substr(config.commandPrefix.length + actualCmd.length + 2);
-		/* 
+		/*
 		Cut out the name of the command to be reloaded
 		INFO: The additional 2 spaces added are the whitespaces between one, the prefix and the command, and two, between the command and the argument.
-		Example: "robbot, reload about" -> cut out the length of the prefix and " reload ". 
+		Example: "robbot, reload about" -> cut out the length of the prefix and " reload ".
 		*/
 		if(arg == "") {
 		// If no command to reload is given...
@@ -160,10 +160,11 @@ bot.on('message', msg => { // Listen to all messages sent
 		// If there is no error, notify the user of success.
 		msg.reply(`command '${cmdFile.slice(0, -3)}' successfully reloaded!`);
 	};
-	process.on("unhandledRejection", err => {
-  		return;
-	});
 	return; // Just in case, return empty for anything else.
+});
+
+process.on("unhandledRejection", err => {
+  	return;
 });
 
 bot.login(config.token); // Log the bot in with token set in config
