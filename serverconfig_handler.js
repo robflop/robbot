7186370e4eb0b343b67fd1082386901d6a975da0
robflop <robflop98@gmail.com>
2017-03-11 13:29:54 +0100

    More trailing space fixes

--- serverconfig_handler.js
+++ serverconfig_handler.js
@@ -4,13 +4,13 @@ var normalizedPath = require("path").join(__dirname, config.serverConfPath); // 
 var serverConfig = {}; // Object of server configs.
 
 // Load all server configs from the serverConfPath (below) -- handler adapted from command handler courtesy of RShadowhand on Github
-fs.readdirSync(normalizedPath).forEach(function(file) { 
+fs.readdirSync(normalizedPath).forEach(function(file) {
 	// Look at all the files in the specificed folder
 	if(file.substr(-5, 5) == ".json") {
 	// If the file is a .json file...
-		var ServerID = file.slice(0, -5).toLowerCase();  
+		var ServerID = file.slice(0, -5).toLowerCase();
 		// ...remove ".json" bit from the file names, convert it to lowercase...
-		serverConfig[ServerID] = require("./"+config.serverConfPath+"/" + file); 
+		serverConfig[ServerID] = require("./"+config.serverConfPath+"/" + file);
 		// ...and then require the files as server config.
 	};
 });
