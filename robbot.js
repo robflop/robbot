a4720dbd08a776e35fc1f0a0bc8fbdfe2c2cd254
robflop <robflop98@gmail.com>
2017-03-25 08:33:02 +0100

    fix dm channels erroring

--- robbot.js
+++ robbot.js
@@ -57,9 +57,9 @@ client.leave = function leaveGuild(id) {
 // shortcut for making bot leave guilds
 
 const handleMsg = (msg) => {
+	if(msg.channel.type !== "text") return msg.channel.send("Commands via (Group) DM not supported, sorry.");
 	const checks = {'cooldown': cooldown, 'botPerm': msg.channel.permissionsFor(client.user), 'userPerm': msg.channel.permissionsFor(msg.member)};
 	if(msg.author.bot || !msg.content.startsWith(config.commandPrefix) || msg.content == config.commandPrefix || checks.cooldown.onCooldown(msg.author.id, msg)) return;
-	if(msg.channel.type !== "text") return msg.channel.send("Commands via (Group) DM not supported, sorry.");
 	if(fs.existsSync(`${config.ignorePath}ignore_${msg.guild.id}.json`) && ignoreLists.ignoreLists[`ignore_${msg.guild.id}`].includes(`${msg.author.id}`)) return;
 	// ignored user check
 	if(fs.existsSync(`${config.serverConfPath}serverconf_${msg.guild.id}.json`) && serverConfig.serverConfig[`serverconf_${msg.guild.id}`].includes(actualCmd)) return;
