2d81f4d5aa407fd063563184470ba9df046d5cdd
robflop <robflop98@gmail.com>
2017-04-24 18:07:58 +0200

    enable pm2 auto-restart instead of relogin

--- eventHandler.js
+++ eventHandler.js
@@ -19,8 +19,8 @@ module.exports = {
 		console.log(`[${timestamp}]${chalk.red("[CONNECTION]")} ${client.user.username} was disconnected! | ${error.code}`);
 		if(error.code == 1000) {
 			console.log(`[${timestamp}]${chalk.green("[POWER]")} Automatically restarting...`);
-			client.destroy().then(() => client.login(config.token));
-			// Restart bot if disconnect code is 1000 (gracefully exited) because it won't reconnect automatically
+			config.pm2 ? process.exit(0) : client.destroy().then(() => client.login(config.token));
+			// Restart selfbot if disconnect code is 1000 (gracefully exited) because it won't reconnect automatically
 		};
 	},
 	"reconnecting": function reconnecting(client, chalk) {
