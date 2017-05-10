53102d42f0b615deec9be6c15570c1aebf9875e4
robflop <robflop98@gmail.com>
2017-05-10 18:15:09 +0200

    update contact info

--- commands/about.js
+++ commands/about.js
@@ -1,7 +1,7 @@
 exports.main = function(client, msg, msgArray, checks, chalk) {
 	const command = "about";
 	if(!checks.botPerm.has('SEND_MESSAGES')) return msg.author.send("I can't send messages to that channel!");
-	msg.channel.send(`robbot made by robflop#0200. Made to complement the website <https://megumin.love> also by robflop#0200.\nCheck out the Github repo at <https://github.com/robflop/megumin.love-discordbot>.\nInvite link for the bot: <https://robflop.pw/robbot>.`);
+	msg.channel.send(`robbot made by robflop#0200. Made to complement the website <https://megumin.love> also by robflop#0200.\nCheck out the Github repo at <https://github.com/robflop/robbot>.\nInvite link for the bot: <https://robflop.me/robbot>.`);
 };
 
 exports.desc = "get general info about the bot";
--- commands/submit.js
+++ commands/submit.js
@@ -1,7 +1,7 @@
 exports.main = function(client, msg, msgArray, checks, chalk) {
 	const command = "submit";
 	if(!checks.botPerm.has('SEND_MESSAGES')) return msg.author.send("I can't send messages to that channel!");
-	msg.reply("Want to submit a sound for the website/bot? No problem, send me an email at `me@robflop.pw` including your cropped mp3/aac/ogg sound file or contact me on discord at robflop#2174! All sounds need to fit the website's theme.");
+	msg.reply("Want to submit a sound for the website/bot? No problem, send me an email at `contact@robflop.me` including your cropped mp3/aac/ogg sound file or contact me on discord at robflop#0200! All sounds need to fit the website's theme.");
 };
 
 exports.desc = "get info on how to submit new sounds for the bot/website";
