757ea168609e4e0bf671dad536ba6c6162d3fc47
robflop <robflop98@gmail.com>
2017-05-13 14:35:56 +0200

    Counter command, writeJSON method

--- src/util/Logger.js
+++ src/util/Logger.js
@@ -1,5 +1,7 @@
 const { blue, cyan, magenta, red, yellow } = require('chalk');
 const moment = require('moment');
+const { writeFile } = require('fs');
+const { join } = require('path');
 
 class Logger {
 	static info(data) {
@@ -16,6 +18,17 @@ class Logger {
 
 	static error(data) {
 		return console.error(`${red(`[${moment().format('YYYY-MM-DD HH:mm:ss')}]`)} ${cyan(data)}`);
+	}
+
+	static writeJSON(content, path, prettified = true) {
+		const joinedPath = join(__dirname, '..', path);
+		const data = prettified ? JSON.stringify(content, null, 4) : JSON.stringify(content);
+		return new Promise((resolve, reject) => {
+			writeFile(joinedPath, data, 'utf-8', error => {
+				if (error) reject(error);
+				else resolve(data);
+			});
+		});
 	}
 }
 
