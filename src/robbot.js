31259aa606ea5f7c5cd18951a871fb4d2e842cb5
robflop <robflop98@gmail.com>
2017-05-14 15:17:37 +0200

    status interval

--- src/robbot.js
+++ src/robbot.js
@@ -8,7 +8,17 @@ const controller = new CommandController();
 client
 	.once('ready', () => {
 		client.logger.info('robbot ready!');
-		client.user.setGame(`try '${client.config.commandPrefix} help' !`);
+		client.user.setGame(`try '${client.config.commandPrefix} help' !`).then(client => {
+			client.setInterval(() => {
+				if (client.user.presence.game.name === `try '${client.config.commandPrefix} help' !`) {
+					client.user.setGame('on megumin.love');
+				}
+				else if (client.user.presence.game.name === 'on megumin.love') {
+					client.user.setGame(`try '${client.config.commandPrefix} help' !`);
+				}
+			// leave untouched if neither of the default ones
+			}, 1000 * 60 * 30);
+		});
 	})
 	.on('message', message => {
 		controller.handleCommand(message);
