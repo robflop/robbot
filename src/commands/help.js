ba8c41ecd1870a65131bf18ace4bf41fe3667034
robflop <robflop98@gmail.com>
2017-05-14 12:53:54 +0200

    Help command

---
+++ src/commands/help.js
@@ -0,0 +1,74 @@
+const Command = require('../structures/Command');
+const { RichEmbed } = require('discord.js');
+
+class HelpCommand extends Command {
+	constructor() {
+		super({
+			name: 'help',
+			description: 'Output a list of commands and help on how to use them',
+			aliases: ['h', 'commands'],
+			args: [
+				{
+					type: 'string',
+					name: 'command',
+					defaultVal: ''
+				}
+			]
+		});
+	}
+
+	async run(message, args, guildConfigs) {
+		const { config: { commandPrefix }, commands, aliases } = message.client;
+		const avatar = message.client.user.avatarURL();
+
+		if (args.command) {
+			const command = commands.get(args.command) || commands.get(aliases.get(command));
+			if (!command) return message.reply(`${args.command} isn't a valid command.`);
+
+			const embed = new RichEmbed()
+			.setAuthor(`robbot Command Help - ${command.name}`, avatar)
+			.setDescription(`**${command.name.capitalize()}** - ${command.description}`)
+			.setColor('RANDOM')
+			.addField('Aliases', command.aliases.join(', ') || 'None', true)
+			.addField('Cooldown', `${command.cooldown} seconds`, true)
+			.addField('Syntax', `${commandPrefix}${command.name} ${command.args.map(arg => `<${arg.name}>`).join(' ')}`, true);
+
+			if (command.ownersOnly) embed.addField('Owners Only', 'Yes', true);
+			if (command.guildOnly) embed.addField('Guild Only', 'Yes', true);
+
+			if (command.perms.client) {
+				embed.addField('Bot Permissions Needed', this.listPermissions(command.perms.client), true);
+			}
+
+			if (command.perms.member) {
+				embed.addField('User Permissions Needed', this.listPermissions(command.perms.member), true);
+			}
+
+			return message.channel.send({ embed });
+		}
+
+		let help = '**__Available Commands:__**\n\n';
+		help += this.listCommands(commands);
+
+		return message.author.send(help, { split: true })
+		.catch(() => message.reply('i ran into an error DM\'ing you!'));
+	}
+
+	listCommands(commands) {
+		let commandsString = '';
+		for (const command of commands.values()) {
+			commandsString += `**${command.name.capitalize()}** - ${command.description}\n`;
+		}
+		return commandsString.slice(0, -1);
+	}
+
+	listPermissions(perms) {
+		let permissionsString = '';
+		for (const perm of perms) {
+			permissionsString += `\`${perm.toString().replace(/_/g, ' ').toLowerCase().toTitleCase()}\`\n`;
+		}
+		return permissionsString.slice(0, -1);
+	}
+}
+
+module.exports = HelpCommand;
