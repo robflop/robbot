87404e984c6b6d2e0bd7dad488b97c4117ce922f
robflop <robflop98@gmail.com>
2017-05-28 21:00:30 +0200

    catch err

--- src/commands/setavatar.js
+++ src/commands/setavatar.js
@@ -21,10 +21,12 @@ class SetAvatarCommand extends Command {
 		const validExts = ['png', 'jpg', 'gif', 'jpeg', 'webp'];
 		const ext = args.url.substr(args.url.lastIndexOf('.') + 1);
 		if (!validExts.includes(ext)) return message.reply('invalid file format! Only PNGs, JP(E)Gs, WebPs and GIFs are accepted.');
-		message.client.user.setAvatar(args.imageURL).then(user => {
-			message.reply(`successfully set my avatar to '<${args.imageURL}>'!`);
+		message.client.user.setAvatar(args.imageURL)
+		.then(user => {
+			message.reply(`successfully set my avatar to \`${args.imageURL}\`!`);
 			message.client.logger.info(`Avatar set to '${args.imageURL}'`);
-		});
+		})
+		.catch(err => message.reply(`an error occurred setting my avatar: \`${err.code}\`!`));
 	}
 }
 
