566e26ad65e53120c3f7d722ca3ceed676c24831
robflop <robflop98@gmail.com>
2017-05-28 20:29:35 +0200

    Make ext scanning better

--- src/commands/setavatar.js
+++ src/commands/setavatar.js
@@ -18,8 +18,9 @@ class SetAvatarCommand extends Command {
 
 	async run(message, args) {
 		if (!args.imageURL.startsWith('http')) return message.reply('invalid URL!');
-		const validFormats = ['.png', '.jpg', '.gif'];
-		if (!validFormats.includes(args.imageURL.substr(-4, 4))) return message.reply('invalid file format! Only pngs, jpgs, gifs are allowed.');
+		const validExts = ['png', 'jpg', 'gif', 'jpeg', 'webp'];
+		const ext = args.url.substr(args.url.lastIndexOf('.') + 1);
+		if (!validExts.includes(ext)) return message.reply('invalid file format! Only PNGs, JP(E)Gs, WebPs and GIFs are accepted.');
 		message.client.user.setAvatar(args.imageURL).then(user => {
 			message.reply(`successfully set my avatar to '<${args.imageURL}>'!`);
 			message.client.logger.info(`Avatar set to '${args.imageURL}'`);
