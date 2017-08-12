b78da2df507a2ca1fe185d7d37e17494683c343e
robflop <robflop98@gmail.com>
2017-08-12 23:19:09 +0200

    don't delete

--- src/commands/power.js
+++ src/commands/power.js
@@ -19,30 +19,28 @@ class PowerCommand extends Command {
 
 	async run(message, args) {
 		if (args.action === 'shutdown') {
-			return message.channel.send('robbot now shutting down! Bye!').then(msg => msg.delete(1000))
-				.then(msg => {
-					message.client.logger.info('robbot shutting down!');
+			return message.channel.send('robbot now shutting down! Bye!').then(msg => {
+				message.client.logger.info('robbot shutting down!');
 
-					if (message.client.config.pm2) {
-						setTimeout(() => exec('pm2 stop robbot'), 2000);
-					}
-					else {
-						setTimeout(() => process.exit(0), 2000);
-					}
-				});
+				if (message.client.config.pm2) {
+					setTimeout(() => exec('pm2 stop robbot'), 2000);
+				}
+				else {
+					setTimeout(() => process.exit(0), 2000);
+				}
+			});
 		}
 
 		else if (args.action === 'restart') {
-			if (!message.client.config.pm2) message.channel.send('Sorry, restarting only works when using pm2!').then(msg => msg.delete(3000));
+			if (!message.client.config.pm2) message.channel.send('Sorry, restarting only works when using pm2!');
 
-			return message.channel.send('robbot now restarting! See you soon!').then(msg => msg.delete(1000))
-				.then(msg => {
-					message.client.logger.info('robbot restarting!');
-					process.exit(0);
-				});
+			return message.channel.send('robbot now restarting! See you soon!').then(msg => {
+				message.client.logger.info('robbot restarting!');
+				process.exit(0);
+			});
 		}
 
-		else return message.channel.send(`'${args.action}' is not a valid action.`).then(msg => msg.delete(3000));
+		else return message.channel.send(`'${args.action}' is not a valid action.`);
 	}
 }
 
