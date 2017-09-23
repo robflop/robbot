d9f44000ca4f1958ddcdc77af4eea67380ad9bd5
robflop <robflop98@gmail.com>
2017-09-23 21:13:04 +0200

    Add reason logging for disconnects

--- src/events/disconnect.js
+++ src/events/disconnect.js
@@ -2,12 +2,12 @@ const { exec } = require('child_process');
 
 function disconnect(client, event) {
 	if (event.code === 1000) {
-		client.logger.info(`Automatically restarting due to WS error code ${event.code}...`);
+		client.logger.info(`Automatically restarting due to WS error code ${event.code} (${event.reason})...`);
 		// Restart if disconnect code is 1000 (gracefully exited) because it won't reconnect automatically
 		return client.config.pm2 ? process.exit(0) : client.destroy().then(() => client.login(client.config.token));
 	}
 	else {
-		client.logger.error(`Disconnected with WS error code ${event.code}, exiting!`);
+		client.logger.error(`Disconnected with WS error code ${event.code} (${event.reason}), exiting!`);
 		// Stop the pm2 task on other critical errors, exit process if no pm2
 		return client.config.pm2 ? exec('pm2 stop robbot') : process.exit(0);
 	}
